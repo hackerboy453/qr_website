cd312ea6d744405e504d134a0a5d8c3a203ebeea
hackerboy453 <hackerboy5916@gmail.com>
2025-09-22 17:48:16 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter, Roboto_Mono } from "next/font/google"
 import "./globals.css"
+import Ads from "@/components/ads"
 
 // Initialize the Inter font for sans-serif text
 const inter = Inter({
@@ -30,7 +31,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${inter.variable} ${roboto_mono.variable} antialiased`}>
-      <body>{children}</body>
+      <body>
+        {children}
+        <Ads />
+      </body>
     </html>
   )
 }
